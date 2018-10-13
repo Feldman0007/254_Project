1c1723dd00c1d9d63fdb51c8e60343565ea301a8
Feldman0007 <35717802+Feldman0007@users.noreply.github.com>
2018-10-12 20:09:37 -0700

    Update prioritizer.h

--- prioritizer.h
+++ prioritizer.h
@@ -1,6 +1,7 @@
 #ifndef PRIORITIZER_H
 #define PRIORITIZER_H
-#include "taskList.h"
+#include "tasklist.h"
+#include <list>
 #include <QMainWindow>
 
 namespace Ui {
@@ -15,16 +16,21 @@ public:
     explicit Prioritizer(QWidget *parent = nullptr);
     ~Prioritizer();
 
-    TaskList taskList;
-    void updateListDisplay(TaskList);
+    void updateListDisplay();
+
+    void sortList(int);
 
 private slots:
     void on_AddTask_clicked();
 
     void on_RemoveTask_clicked();
 
+    void on_SortList_clicked();
+
 private:
     Ui::Prioritizer *ui;
+    list<Task> taskList;
+    list<Task>::iterator cursor;
 };
 
 #endif // PRIORITIZER_H
