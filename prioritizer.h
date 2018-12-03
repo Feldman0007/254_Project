96ad9695cc70abf3962aa0b3de11b3d967e52f56
Feldman0007 <35717802+Feldman0007@users.noreply.github.com>
2018-12-02 23:27:45 -0800

    Update prioritizer.h

--- prioritizer.h
+++ prioritizer.h
@@ -37,7 +37,7 @@ private:
     Ui::Prioritizer *ui;    // initializes the gui
     list<Task> taskList;    // taskList will be a list of all tasks entered by the user so that they may be re-organized to help in prioritizing tasks
     list<Task>::iterator cursor;        // this iterator will be used to cycle through the contents of the current assignment list
-    bool save_the_file = true;      //flag variables that wll be used for saving, loading, and creating new files
+    bool save_the_file = false;      //flag variables that wll be used for saving, loading, and creating new files
     QString currentFileName = "";    // this variable will keep track of the current working file
 };
 
